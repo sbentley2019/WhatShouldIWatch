e28a35098a002474202cebfa855d63b31781471e
sbentley2019 <sbentley000@gmail.com>
2020-04-07 13:10:06 -0400

    add react app and simple server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>");
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
